6ec4bcb49a30fe22742cc9225415adad8a1e0b70
Kumar <kumar0616@hotmail.com>
2017-08-27 23:06:52 -0700

    Changes in input path

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -7,7 +7,7 @@ object WordCount {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("Word Counter")
     val sc = new SparkContext(conf)
-    val textFile = sc.textFile("file:///usr/spark/README.md")
+    val textFile = sc.textFile("file:///usr/local/spark/README.md")
     val tokenizedFileData = textFile.flatMap(line => line.split(" "))
     val countPrep = tokenizedFileData.map(word => (word, 1))
     val counts = countPrep.reduceByKey((accumValue, newValue) => accumValue + newValue)
